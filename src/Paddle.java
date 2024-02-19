8f73ae6c65103a81fe4e041bbbd39dc8376bac1f
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-19 11:50:40 +0100

    Methods for paddle

--- src/Paddle.java
+++ src/Paddle.java
@@ -9,4 +9,24 @@ public class Paddle {
 		
 	}
 	
+	public void keyPressed(KeyEvent e) {
+		
+	}
+	
+	public void keyReleased(KeyEvent e) {
+		
+	}
+	
+	public void setYDirection(int yDirection) {
+		
+	}
+	
+	public void move() {
+		
+	}
+	
+	public void draw(Graphics g) {
+		
+	}
+	
 }
