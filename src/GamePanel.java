8a20bafd501d2646b235a54c28c6057b03e865ae
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-20 14:18:38 +0100

    making ball bounce off top and bottom of the window game

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -64,6 +64,15 @@ public class GamePanel extends JPanel implements Runnable{
 	}
 	
 	public void checkCollision() {
+		
+		// bounce ball off top and bottom window edges
+		if(ball.y <= 0) {
+			ball.setYDirection(-ball.yVelocity);
+		}
+		if(ball.y >= GAME_HEIGHT-BALL_DIAMETER) {
+			ball.setYDirection(-ball.yVelocity);
+		}
+		
 		// stops paddles at window edges
 		if(paddle1.y<=0) paddle1.y = 0;
 		if(paddle1.y >= (GAME_HEIGHT-PADDLE_HEIGHT)) paddle1.y = GAME_HEIGHT-PADDLE_HEIGHT;
