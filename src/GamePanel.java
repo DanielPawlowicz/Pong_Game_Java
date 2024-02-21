4760eac2e7c4371cc9076704909478c0cbb98271
Daniel Pawłowicz <danielpawlowicz@interia.pl>
2024-02-21 09:36:54 +0100

    Making ball start in random height

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -35,7 +35,7 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	public void newBall() {
 		random = new Random();
-		ball = new Ball((GAME_WIDTH/2)-(BALL_DIAMETER/2),(GAME_HEIGHT/2)-(BALL_DIAMETER/2),BALL_DIAMETER, BALL_DIAMETER);
+		ball = new Ball((GAME_WIDTH/2)-(BALL_DIAMETER/2),random.nextInt(GAME_HEIGHT-BALL_DIAMETER),BALL_DIAMETER, BALL_DIAMETER);
 	}
 	
 	public void newPaddles() {
